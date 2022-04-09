d2a334b96a8b4a19760979be10abb63ffd86890f
danieltafur23 <79168401+danieltafur23@users.noreply.github.com>
2022-04-09 07:34:46 -0500

    NTV se avanza en el codigo

--- clases/Ciclista.py
+++ clases/Ciclista.py
@@ -1,42 +1,39 @@
 class Ciclista:
-
     def __init__(self):
-        self.__nombre=None
-        self.__edad=None
-        self.__pais=None
-        self.__tiempo=None
-
-    @property
-    def nombre(self):
-        return self.__nombre
-
-    @property
-    def edad(self):
-        return self.__edad
-
-    @property
-    def pais(self):
-        return self.__pais
-
-    @property
-    def tiempo(self):
-        return self.__tiempo
-
-    @nombre.setter
-    def nombre(self,nombre):
-        self.__nombre=nombre
+        self.__name=None
+        self.__age=None
+        self.__country=None
+        self.__time=None
     
-    @edad.setter
-    def edad(self,edad):
-        if(edad < 0):
-            print("Digite edad valida no permito numeros negativos")
-        else:
-            self.__edad=edad
+    @property
+    def name(self):
+        return self.__name
 
-    @pais.setter
-    def pais(self,pais):
-        self.__pais=pais
+    @property
+    def age(self):
+        return self.__age
 
-    @tiempo.setter
-    def tiempo(self,tiempo):
-        self.__tiempo=tiempo
+    @property
+    def country(self):
+        return self.__country
+
+    @property
+    def time(self):
+        return self.__time
+    
+    #setters
+    @name.setter
+    def name(self,name):
+        self.__name=name
+
+    @age.setter
+    def age(self,age):
+        self.__age=age
+
+    @country.setter
+    def country(self,country):
+        self.__country=country
+
+    @time.setter
+    def time(self,time):
+        self.__time=time
--- main.py
+++ main.py
@@ -1,13 +1,63 @@
-from clases.Ciclista import Ciclista
+from Ciclista import Ciclista
 
-Ciclista=Ciclista()
+def menu():
+    return int(input(
+"""
+-------------- MENU ------------
+1. ingresar ciclista
+2. mostrar ciclista con mejor tiempo
+3. salir
+"""
+    ))
 
-Ciclistas = []
+def agg():
+    ciclista={
+        'name': input('nombre: '),  
+        'age':int(input('edad: ')),
+        'country':input('pais: '),
+        'time':float(input('time: '))  
+    }
+    return ciclista
 
-cantidad= int (input("Digite la cantidad de ciclistas"))
+def mejor_ciclista(ciclistas):
 
-for i in range(cantidad):
-    Ciclista.nombre=(input("Digite nombre: "))
-    Ciclista.edad=int(input("Digite la edad: "))
-    Ciclista.pais=(input("Digite el pais: "))
-    Ciclista.tiempo=int(input("Digite el tiempo: "))
+    mejor=None
+    posicion=0
+    if len(ciclistas) != 0:
+        for ciclista in ciclistas:
+            if mejor==None:
+                mejor=ciclista.time
+                posicion=ciclistas.index(ciclista)
+            elif ciclista.time < mejor:
+                mejor=ciclista.time
+                posicion=ciclistas.index(ciclista)
+        return posicion
+    return print("No hay ningun ciclista")
+
+ciclistas=[]
+salir=False
+
+while not salir:
+    opcion=menu()
+    if opcion==1:
+        ciclista=agg()
+        ciclistas.append(Ciclista())
+        ciclistas[-1].name=ciclista['name']
+        ciclistas[-1].age=ciclista['age']
+        ciclistas[-1].country=ciclista['country']
+        ciclistas[-1].time=ciclista['time']
+    elif opcion==2:
+        posicion=mejor_ciclista(ciclistas)
+        print(
+f"""
+nombre: {ciclistas[posicion].name}
+edad: {ciclistas[posicion].age}
+pais: {ciclistas[posicion].country}
+time: {ciclistas[posicion].time}
+"""
+        )
+    elif opcion==3:
+        salir=1
+    
+    else:
+        print("Opcion incorrecta >.<")
